3dc75e8f7fcc2f59af8ba345bf44792e7ea36c47
Duplication1 <gamot.kim.fernandez@gmail.com>
2024-12-29 14:53:25 +0800

    pagination and seeders

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,8 +1,16 @@
 <x-layout>
     <x-slot:heading>jobs</x-slot:heading>
-    <ul>
+    <div class="space-y-4">
     @foreach ($jobs as $job)
-        <li><a href="/jobs/{{$job['id']}}"> {{ $job['title'] }} earns {{ $job['salary']}} per year </a></li>
+    <a href="/jobs/{{$job['id']}}" class="hover:underline block px-4 py-6 border border-gray-200">
+        <div class="text-blue-500">{{$job->employer->name}}</div>
+        <div>
+            {{ $job['title'] }} earns {{ $job['salary']}} per year
+        </div>
+        </a>
     @endforeach
-    </ul>
+    <div>
+        {{ $jobs->links() }}
+    </div>
+    </div>
 </x-layout>
