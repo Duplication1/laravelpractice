5f0b094322f9e415f45048bcdd1ba794734b2451
Duplication1 <gamot.kim.fernandez@gmail.com>
2025-01-02 15:24:27 +0800

    controller

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,4 +2,8 @@
     <x-slot:heading>jobs</x-slot:heading>
     <H2>{{ $job['title'] }}</H2>
     <H2>{{ $job['salary'] }}</H2>
+
+    
+        <a href="/jobs/{{$job->id}}/edit" class="bg-blue-500">Edit</a>
+    
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 
 
 Route::get('/', function () {
@@ -12,35 +12,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
 
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required', 'numeric']
-    ]);
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-Route::get('/jobs/{id}', function ($id){
-    $job = Job::find($id);
-    return view('job.show', ['job' => $job]);
-});
-
+Route::resource('jobs', JobController::class);
